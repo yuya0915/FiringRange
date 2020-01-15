7d40a38508407e21be1e7a15a41688e40c47c45b
yuya0915 <nkc.yuya.mase.2016@gmail.com>
2020-01-15 14:56:46 +0900

    基本設定

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform _camTransform;//cameraのtransformer
+
+    private Vector3 _startMousePos;//マウス操作の始点
+
+    private Vector3 _presentCamRotation;//Cameraの回転の始点情報
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camTransform = this.gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraRotationMouseControl();
+    }
+
+    private void CameraRotationMouseControl()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    CharacterController characterController;
+    public float speed;
+    public float jumpSpeed;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        characterController.Move(moveDirection * Time.deltaTime);
+
+
+            //if (Input.GetKey(KeyCode.W))
+            //{
+            //    transform.position += transform.forward * speed * Time.deltaTime;
+            //}
+            //if (Input.GetKey(KeyCode.S))
+            //{
+            //    transform.position -= transform.forward * speed * Time.deltaTime;
+            //}
+            //if (Input.GetKey(KeyCode.D))
+            //{
+            //    transform.position += transform.right * speed * Time.deltaTime;
+            //}
+            //if (Input.GetKey(KeyCode.A))
+            //{
+            //    transform.position -= transform.right * speed * Time.deltaTime;
+            //}
+        }
+}
