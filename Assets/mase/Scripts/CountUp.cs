8d8f653dc9c79610e7eaf97e3b354ea30b5af4fc
yuya0915 <nkc.yuya.mase.2016@gmail.com>
2020-01-31 15:03:20 +0900

    マズルフラッシュ製作中

--- Assets/mase/Scripts/CountUp.cs
+++ Assets/mase/Scripts/CountUp.cs
@@ -5,7 +5,8 @@ using UnityEngine.UI;
 
 public class CountUp : MonoBehaviour
 {
-    private float countup = 0.0f;
+    private float timecount = 0.0f;
+
 
     public Text timetext;
     // Start is called before the first frame update
@@ -17,8 +18,10 @@ public class CountUp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        countup += Time.deltaTime;
 
-        timetext.text = countup.ToString("f1") + "秒";
+
+        timecount += Time.deltaTime;
+
+        timetext.text = timecount.ToString("f1") + "秒";
     }
 }
--- Assets/mase/Scripts/RayControl.cs
+++ Assets/mase/Scripts/RayControl.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RayControl : MonoBehaviour
 {
 
     public Camera camera_obj;//Cameraを取得
     private RaycastHit hit;//レイが当たったモノを取得
+
+    public Text Hittext;//テキスト
+    public float HitCount;//当たったカウント
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +27,8 @@ public class RayControl : MonoBehaviour
             if (Physics.Raycast(camera_obj.transform.position,transform.forward,out hit,5))
             {
                 Destroy(hit.collider.gameObject);
+                HitCount += 1;//消したカウント
+                Debug.Log(HitCount);
                 Debug.DrawRay(camera_obj.transform.position, transform.forward, Color.blue);
             }
         }
