e2eab2928d4b202541b4c5b2e28cf67d0e44ba75
yuya0915 <nkc.yuya.mase.2016@gmail.com>
2020-01-31 11:39:58 +0900

    Cursorロック

--- Assets/mase/Scripts/CursorController.cs
+++ Assets/mase/Scripts/CursorController.cs
@@ -8,6 +8,7 @@ public class CursorController : MonoBehaviour
     void Start()
     {
         Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
