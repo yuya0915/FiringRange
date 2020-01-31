99fa93179e6055869158d9f8d5094c0a6c8d6d40
yuya0915 <nkc.yuya.mase.2016@gmail.com>
2020-01-31 10:40:37 +0900

    カウントアップ追加

---
+++ Assets/mase/Scripts/CountUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountUp : MonoBehaviour
+{
+    private float countup = 0.0f;
+
+    public Text timetext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countup += Time.deltaTime;
+
+        timetext.text = countup.ToString("f1") + "秒";
+    }
+}
